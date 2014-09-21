3a847de365e370c6773d5084f8e380afeb14c7bd
frogeyedpeas <sid.ghoshal@yahoo.com>
2014-09-21 12:06:01 -0400

    a

--- python-stackexchange-api/revisedfolder/tagproduction.py
+++ python-stackexchange-api/revisedfolder/tagproduction.py
@@ -40,6 +40,7 @@ def tagparsing(theobject1, theobject2): #theobject1 is the xml tree, the object2
 	k = 0
 	equalizer = 0 #matching up the write value
 	tempstring = "buklauu"
+	subtractor = 0
 	for nodes, k in zip(theobject1, range(len(theobject1))):
 		i = 0
 		
@@ -51,11 +52,12 @@ def tagparsing(theobject1, theobject2): #theobject1 is the xml tree, the object2
 				if(TagArray[i][:-5] in str(nodes.attrib['Tags'])):
 					
 					
-					tempstring = str(equalizer + k)+":{\"color\":\"Blue\",\"shape\": \"dot\",\"label\":\"Strings\", \"size\": 60, \"isExploded\":false, \"precedence\": 2," + TagArray[i] + "},"
+					tempstring = "\"" + str(equalizer + k)+"\":{\"color\":\"Blue\",\"shape\": \"dot\",\"label\":\"Strings\", \"size\": 60, \"isExploded\":false, \"precedence\": 2," + TagArray[i] + "},"
 					
 					fileArray[i].write(tempstring)
 			except:
 
+				equalizer = equalizer - 1
 				pass		
 				
 				
@@ -71,7 +73,7 @@ def tagparsing(theobject1, theobject2): #theobject1 is the xml tree, the object2
 
 	
 	while(i < len(fileArray)-1): #simultaneously writes to last file arrray and closes files
-		tempstring = str(equalizer + i) + ":{\"color\":\"Blue\",\"shape\":\"dot\",\"label\":\"Strings\", \"size\": " +  str(os.path.getsize("C:\Users\Sidharth\Documents\GitHub\DownloadsNSFW\cs.stackexchange.com\TheG20\\" + dirlist[i])) + ", \"isExploded\":false, \"precedence\": 2, + \"filename\" : \"" + TagArray[i]+ "\"},"
+		tempstring = "\"" + str(equalizer + i) + "\":{\"color\":\"Blue\",\"shape\":\"dot\",\"label\":\"Strings\", \"size\": " +  str(os.path.getsize("C:\Users\Sidharth\Documents\GitHub\DownloadsNSFW\cs.stackexchange.com\TheG20\\" + dirlist[i])) + ", \"isExploded\":false, \"precedence\": 2, + \"filename\" : \"" + TagArray[i]+ "\"},"
 	
 		fileArray[len(fileArray)-1].write(tempstring)
 		fileArray[i].close()
