14812de7727f7f11abe83cf19491f7c4708d5bec
brugel18@gmail.com <brugel18@gmail.com>
2014-09-20 23:14:15 -0400

    asdf

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as xml
 
 pitfalls = ["long", "difficult", "hard", "understand", "formidable", "challenging", "troublesome", "confused", "test", "tried"]
-beginner = ["new", "first", "time", "never", "hw", "easy"]
+beginner = ["beginner", "elementry", "sorry", "new", "first", "time", "never", "hw", "easy"]
 
 def scorer(weights, child):
     score = 0
@@ -29,21 +29,25 @@ def scorer(weights, child):
                 score += weights[4]
     except:
         pass
+    try:
+        user = child.attrib['OwnerUserId']
+        for x in userRoot:
+            if x.attrib['Id'] == user:
+                score += weights[5]*float(x.attrib['Reputation'])
+    except:
+        pass
     return score
 
 def scorer_difficult_problems(child):
-    weights = [50, 10, 10, 200, 10]
+    weights = [50, 10, 10, 200, -100, 1]
     return scorer(weights, child)
 
 def scorer_common_pitfall(child):
-    weights = [100, 50, 50, 100, 200]
+    weights = [100, 50, 50, 100, 200, -1]
     return scorer(weights, child)
 
 users = xml.parse('../stackdata/cs.stackexchange.com/Users.xml')
 userRoot = users.getroot()
-
-
-
 
 
 
@@ -60,5 +64,6 @@ for child in root:
         pass
 r = sorted(ranks.iteritems(), key=lambda (k, v): -v )
 r = [child[0].attrib  for child in r[:10]]
+
 for x in r:
     print x
